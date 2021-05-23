141a03cc8bc6088ef740e253bee688cbc923edff
Cody Johnson <codyj@protonmail.com>
2021-05-23 15:29:23 -0400

    Add app initializer

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// App contains things for the app to work
+type App struct {}
+
+// Run initializes the application
+func (app *App) Run() error {
+	fmt.Println("Initializing application...")
+	return nil
+}
+
 func main() {
-	fmt.Println("Go REST API")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting application")
+		fmt.Println(err)
+	}
 }
